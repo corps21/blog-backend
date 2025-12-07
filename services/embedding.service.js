6ce8314fde0aaae649375c9e890de8109a89f546
corps21 <tatya-bicchu-21@proton.me>
2025-12-07 09:54:15 +0530

    feat: integrate @xenova/transformers for embedding service
    - Added @xenova/transformers dependency to package.json.
    - Refactored EmbeddingService to utilize the pipeline from @xenova/transformers for text embeddings.
    - Removed unnecessary API key and base URL parameters, simplifying the service initialization.

--- services/embedding.service.js
+++ services/embedding.service.js
@@ -1,40 +1,14 @@
-import { tryCatchWrapper } from "../utils/index.js";
-
-const MODEL = "models/gemini-embedding-001";
-const BASE_URL =
-	"https://generativelanguage.googleapis.com/v1beta/models/gemini-embedding-001:embedContent";
+import { pipeline } from "@xenova/transformers"
 
 class EmbeddingService {
-	constructor(model, apiKey, baseUrl) {
-		this.model = model;
-		this.apiKey = apiKey;
-		this.baseUrl = baseUrl;
-	}
-
 	getEmbedding = tryCatchWrapper(async (text) => {
-		const headers = {
-			"Content-Type": "application/json",
-			"x-goog-api-key": this.apiKey,
-		};
+		const embedder = await pipeline(
+			'feature-extraction',
+			'Xenova/nomic-embed-text-v1');
 
-		const body = JSON.stringify({
-			model: this.model,
-			content: {
-				parts: [{ text }],
-			},
-		});
-
-		const embeddingResponse = await fetch(this.baseUrl, {
-			headers,
-			body,
-		});
-		const embeddingData = await embeddingResponse.json();
-		return embeddingData;
+		const results = await embedder(text, { pooling: 'mean', normalize: true });
+		return Array.from(results.data);
 	});
 }
 
-export const embeddingService = new EmbeddingService(
-	MODEL,
-	process.env.GEMINI_API_KEY,
-	BASE_URL,
-);
+export const embeddingService = new EmbeddingService();
