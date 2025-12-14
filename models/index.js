05a17a77e425d1376cd4662ed7a0ef2c3f92fc82
corps21 <tatya-bicchu-21@proton.me>
2025-12-14 19:30:33 +0530

    Revert "feat: add Notebook model and integrate with save functionality"
    This reverts commit 302f31d7f1a10d0fd5dda6821e139322787e97e1.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 import { Post } from "./post.model.js";
 import { AnonUser, User } from "./user.model.js";
-import { Notebook } from "./notebook.model.js";
 
-export { User, Post, AnonUser, Notebook };
+export { User, Post, AnonUser };
